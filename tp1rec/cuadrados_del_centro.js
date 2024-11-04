35267a07d199ff9e39d2bd4904d52280ae457f6d
ianurbanski <166758728+ianurbanski@users.noreply.github.com>
2024-11-04 14:20:28 -0300

    Add files via upload

---
+++ tp1rec/cuadrados_del_centro.js
@@ -0,0 +1,8 @@
+//dibuja los cuadrados del centro
+
+function dibujarCuadradosDelCentro(tamaño) {
+  rect(100, 100, tamaño, tamaño );
+  rect(300, 100,  tamaño, tamaño);
+  rect(100, 300,  tamaño, tamaño);
+  rect(300, 300,  tamaño, tamaño);
+}
